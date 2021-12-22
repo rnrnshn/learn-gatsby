c694a7df75bccda69153d624b77dd07460eec4c7
rurushu <olymangue@gmail.com>
2021-12-22 15:14:09 +0200

    finish gatsby tutorial part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "learn-gatsby",
   },
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp'
+  ],
 };
